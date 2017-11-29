cea197f5a23e2fa3920bcac7d32186aa828d2317
usaima <usaimarif007@gmail.com>
2017-11-29 16:51:08 +0500

    Update LogisticRegression_Exercise.py

--- LogisticRegression_Exercise.py
+++ LogisticRegression_Exercise.py
@@ -7,11 +7,12 @@ Created on Wed Nov 29 16:03:45 2017
 import numpy as np
 from sklearn import datasets
 from sklearn import linear_model
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 
 iris = datasets.load_iris()
-iris_X = iris.data
+iris_X = iris.data[: , :2]
+#taking first 2 features only
 iris_y = iris.target
 np.unique(iris_y)
 #to split data randomly
@@ -26,6 +27,9 @@ logistic = linear_model.LogisticRegression(fit_intercept=True , C=1e5)
 logistic.fit(iris_X_train, iris_y_train)
 
 print (logistic.intercept_, logistic.coef_)
+
+plt.figure(figsize = (12, 8))
+plt.scatter( iris_X_train[:,0], iris_X_train[:,1] , c = iris_y_train, alpha = .8, s = 50)
 
 """
 [  0.90253316   6.98000716 -38.13251869] [[  1.51252244   4.93369151  -7.81700689  -3.83849959]
